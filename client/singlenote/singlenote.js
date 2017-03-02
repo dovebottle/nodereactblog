79f1b81399d3953d7d63008376411b5d69d766c1
dove <313474186@qq.com>
2017-03-02 11:13:36 +0800

    文章页

--- client/singlenote/singlenote.js
+++ client/singlenote/singlenote.js
@@ -3,30 +3,77 @@ import styles from './singlenote.scss';//scss导入
 
 var $ = require('jquery');
 var add_commen = {
-	addtime: "",
+	addtime: "",//canvas唯一标识
 	ctx: '',
 	flag: false,
 	clickUpX: "",
 	clickUpY: "",
 	clickDownX: "",
 	clickDownY: "",
+	pencilX: 0,
+	pencilY: 0,
+	boxID: '',
 
 	init: function(add_id, conent_id) {
 		var _this = this;
+		this.boxID = conent_id;
 		this.listen_creat(add_id, conent_id);
 		this.listen_pencil();
 		$('#close').click(function() {
 			_this.close_and_clean();
 		});
 		$('#window_add').click(function() {
-			_this.close_and_clean();
+			var getLocation;
+			getLocation = true;//该数据为划取坐标，暂时用true代替？？？
 
-			var newbulb = document.getElementById("content_jq");
-			var newbulbDiv = $("<div class='addnewBulb' data-comment='評論' data-bulbid='"+_this.addtime+"'></div>");
-			$('#content_jq').append(newbulbDiv);
+			var content = $('.window_content').val();
+			console.log(content);
+			if (content) {
+				// canUseCreateButton = true;
+				var timeflag = _this.addtime;   //作为唯一匹配id	
+				//添加灯泡图标
+				var bulbLeft = _this.pencilX;
+				var bulbTop = _this.pencilY;
 
-//不见z-index，不在最顶层，此时会闪烁变换
-			newbulbDiv.css({"z-index": 1000, "left" : _this.clickDownX+3 , "top": _this.clickDownY-10, "click" :_this.showclickdialog() ,"hover": _this.showhoverdialog() });
+				var newbulb = document.getElementById("content_jq");
+				var newbulbDiv = $("<div class='addnewBulb' id='"+timeflag+"' data-comment='"+content+"' data-bulbid='"+_this.addtime+"'></div>");
+				$('#content_jq').append(newbulbDiv);
+
+	//不见z-index，不在最顶层，此时会闪烁变换
+				newbulbDiv.css({"z-index": 1000, "left" : _this.clickDownX+3 , "top": _this.clickDownY-10, "click" :_this.showclickdialog() ,"hover": _this.showhoverdialog() });
+
+				//到这000000
+				// var newbulb = document.getElementById("paragraph");
+				// var newbulbDiv = document.createElement("div");
+				// newbulbDiv.className = "addnewBulb";
+				// newbulbDiv.id = timeflag;
+				// newbulbDiv.hover = showhoverdialog();
+				// newbulb.appendChild(newbulbDiv);
+				// $("div[id="+timeflag+"]").attr("data-comment",value);
+	//不见z-index，不在最顶层，此时会闪烁变换
+				// $("div[id="+timeflag+"]").css({"z-index": 1000,"left" : bulbLeft+3 , "top" : bulbTop-10,"click" :showclickdialog() ,"hover":showhoverdialog() });
+				
+				$("canvas[id="+timeflag+"]").css({"display":"none"});
+
+				//评论列表的插入
+				var $list = $(".list");
+				var $postil = $("<div forid='"+timeflag+"'>"+content+"</div>");
+				$list.append($postil);
+				$postil.hover(function(){
+					$(this).removeClass().addClass("postilBarFocus");
+					var gethoverid = $(this).attr("forid");
+					$("canvas[id="+gethoverid+"]").css({"display":"block"});
+				},function(){
+					$(this).removeClass().addClass("postilBar");
+					var gethoverid = $(this).attr("forid");
+					$("canvas[id="+gethoverid+"]").css({"display":"none"});
+				});
+
+				_this.close_and_clean();
+			} else {
+				alert('点开我什么都不说就想走？惊了！');
+			}
+
 		});
 
 	},
@@ -110,6 +157,8 @@ var add_commen = {
 		console.log(event);
 		var pencilX = event.offsetX;
 		var pencilY = event.offsetY;
+		this.pencilX = pencilX;
+		this.pencilY = pencilY;
 		$("#pencilIcon").css({
 			"width":25,
 			"height":25,
@@ -130,10 +179,41 @@ var add_commen = {
 		$("#comment_window").css({"display" :"none"});
 	},
 	showclickdialog: function() {
-
+		console.log('asdf');
+		$("div[class=addnewBulb]").click(function(e){
+			// var clickdialogheight = e.clientY + document.documentElement.scrollTop - 60;
+			// console.log(clickdialogheight);
+			// $("div[id=listbar]").css({"visibility":"visible","margin-top":clickdialogheight });
+			$("div[id=listbar]").css({"visibility":"visible"});
+		});
 	},
 	showhoverdialog: function() {
+		$("div[class=addnewBulb]").hover(function(e){
+			var getid = $(this).attr("id");
+			$("div[forid="+getid+"]").removeClass().addClass("postilBarFocus");
+			$("canvas[id="+getid+"]").css({"display":"block"});
+			var showLeft = $(this).position().left;
+			var showTop = $(this).position().top;
+			// console.log(showLeft);
+			var text = $(this).attr("data-comment");
+			var $hoverdialog = $("#hoverdialog");
+			var $exit = $("div[selecthover="+getid+"]")[0];  //为什么要加个【0】
+			if (!$exit){
+				var $hoverdialogDiv = $("<div selecthover="+getid+" class=hoverdialogcss>"+text+"</div>");
+				$hoverdialog.append($hoverdialogDiv);
+			}
+			var hoverheight = $("div[selecthover="+getid+"]")[0].offsetHeight;
+			var hoverwidth = $("div[selecthover="+getid+"]")[0].offsetWidth;
+			// console.log(hoverheight);
+			// console.log(hoverwidth);
+			$("div[selecthover="+getid+"]").css({"left" : showLeft-(hoverwidth / 2) , "top" : showTop-hoverheight-10 , "visibility" : "visible"});
 
+		},function(e){
+			var getid = $(this).attr("id");
+			$("div[forid="+getid+"]").removeClass().addClass("postilBar");
+			$("canvas[id="+getid+"]").css({"display":"none"});
+			$("div[selecthover="+getid+"]").css({"visibility" : "hidden"});
+		});
 	}
 };
 
